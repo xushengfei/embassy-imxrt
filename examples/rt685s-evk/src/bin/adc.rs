cd9ddba7349ed296d65d2df82667c48ce7dd1c30
jerrysxie <jerryxie@microsoft.com>
2024-08-05 11:14:06 -0500

    Add initial ADC HAL that supports single sample
    * Run ADC off LPOSC until clock tree is finalized
    
    * Add placeholder pin configuration code until GPIO HAL is finalized
    
    * Only single channel is working due to command chain documented in
    datasheet is not working as expected. Engaging with NXP.
    
    * Update ADC example to exercise ADC HAL

--- examples/rt685s-evk/src/bin/adc.rs
+++ examples/rt685s-evk/src/bin/adc.rs
@@ -4,14 +4,27 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_imxrt::adc::Adc;
+use embassy_imxrt::adc::ChannelConfig;
+use embassy_imxrt::adc::Config;
+use embassy_imxrt::adc::InterruptHandler;
+use embassy_imxrt::bind_interrupts;
+
+bind_interrupts!(struct Irqs {
+    ADC0 => InterruptHandler;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_imxrt::init(Default::default());
-    let _adc = Adc::new(p.ADC0);
+    let channel_config = [ChannelConfig::single_ended(p.P0_05)];
+    let mut adc = Adc::new(p.ADC0, Irqs, Config::default(), channel_config);
 
     loop {
-        info!("I am the ADC example");
-        embassy_imxrt_examples::delay(50_000_000);
+        let mut data: [i16; 1] = [0; 1];
+        adc.sample(&mut data).await;
+
+        info!("ADC sample = {:#x}", data);
+
+        embassy_imxrt_examples::delay(50000);
     }
 }
