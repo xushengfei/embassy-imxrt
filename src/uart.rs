7b7565c1f2c61dd42cf0b127658017a8f9136ad1
Akshay Mahajan <akmahaj@microsoft.com>
2024-08-06 21:54:35 +0530

    Added code for Uart Init and Uart Open()

--- src/uart.rs
+++ src/uart.rs
@@ -1,4 +1,6 @@
 //! UART driver
+
+use core::cell::Cell;
 use mimxrt685s_pac as pac;
 
 use crate::pac::dma0;
@@ -11,6 +13,16 @@ use crate::pac::usart0;
 
 use flexcomm0::pselid::Persel as PeripheralType;
 
+const UART_CTRL_FLAG_ENABLE_TX: u32 = 1u32 << 0;
+const UART_CTRL_FLAG_ENABLE_RX: u32 = 1u32 << 1;
+const UART_CTRL_FLAG_ENABLE_FLOWCTRL: u32 = 1u32 << 2;
+const UART_CTRL_FLAG_ENABLE_LOOPBACK_TEST: u32 = 1u32 << 3;
+
+const UART_STAT_FLAG_FLAG_OPEN: u32 = 1u32 << 0;
+const UART_STAT_FLAG_FLAG_RX_ACTIVE: u32 = 1u32 << 1;
+const UART_ACTION_RX_START: u32 = 1u32 << 0;
+const UART_ACTION_RX_END: u32 = 1u32 << 1;
+const UART_ACTION_NEW_RX_BYTES: u32 = 1u32 << 2;
 //use crate::pac::dma1;
 //use crate::{pac};
 //use crate::{pac, Peripheral};
@@ -18,14 +30,14 @@ use flexcomm0::pselid::Persel as PeripheralType;
 /// A GPIO port with up to 32 pins.
 //#[derive(Debug, Eq, PartialEq)]
 pub enum FlexComm {
-    eFLEXCOMM_HOST_UART = 0,
-    eFLEXCOMM_DEBUG_UART      = 1,
-    eFLEXCOMM_VFG_I2C         = 2,
-    eFLEXCOMM_TDM_UART        = 3,
+    FlexcommHostUart = 0,
+    FlexcommDebugUart      = 1,
+    FlexcommVfgI2c         = 2,
+    FlexcommTdmUart        = 3,
 
     /// Interface to KIP, Blade and Touchpad
-    eFLEXCOMM_TOUCHPAD_UART   = 4, 
-    eFLEXCOMM_SURFLINK_UART   = 5,
+    FlexcommTouchpadUart   = 4, 
+    FlexcommSurflinkUart   = 5,
    // eFLEXCOMM_TEMP_SENSOR_I2C = 6,
 
      // eFLEXCOMM_IMU_SENSOR_I2C might be redundant. Need investigation
@@ -39,12 +51,12 @@ impl From<u8> for FlexComm {
         use FlexComm::*;
 
         match value {
-            0 => eFLEXCOMM_HOST_UART,
-            1 => eFLEXCOMM_DEBUG_UART,
-            2 => eFLEXCOMM_VFG_I2C,
-            3 => eFLEXCOMM_TDM_UART,
-            4 => eFLEXCOMM_TOUCHPAD_UART,
-            5 => eFLEXCOMM_SURFLINK_UART,
+            0 => FlexcommHostUart,
+            1 => FlexcommDebugUart,
+            2 => FlexcommVfgI2c,
+            3 => FlexcommTdmUart,
+            4 => FlexcommTouchpadUart,
+            5 => FlexcommSurflinkUart,
            // 6 => eFLEXCOMM_TEMP_SENSOR_I2C,
            // 7 => eFLEXCOMM_IMU_SENSOR_I2C,
            // 8 => eFLEXCOMM_UEFI_TP_SPI,
@@ -57,12 +69,12 @@ impl From<u8> for FlexComm {
 impl From<FlexComm> for u8 {
     fn from(value: FlexComm) -> Self {
         match value {
-            FlexComm ::eFLEXCOMM_HOST_UART => 0,
-            FlexComm::eFLEXCOMM_DEBUG_UART => 1,
-            FlexComm::eFLEXCOMM_VFG_I2C => 2,
-            FlexComm::eFLEXCOMM_TDM_UART => 3,
-            FlexComm::eFLEXCOMM_TOUCHPAD_UART => 4,
-            FlexComm::eFLEXCOMM_SURFLINK_UART => 5,
+            FlexComm ::FlexcommHostUart => 0,
+            FlexComm::FlexcommDebugUart => 1,
+            FlexComm::FlexcommVfgI2c => 2,
+            FlexComm::FlexcommTdmUart => 3,
+            FlexComm::FlexcommTouchpadUart => 4,
+            FlexComm::FlexcommSurflinkUart => 5,
             //FlexComm::eFLEXCOMM_TEMP_SENSOR_I2C => 6,
             //FlexComm::eFLEXCOMM_IMU_SENSOR_I2C => 7,
             //FlexComm::eFLEXCOMM_UEFI_TP_SPI => 8,
@@ -73,33 +85,31 @@ impl From<FlexComm> for u8 {
 
 
 impl FlexComm{
-   // fn regs(&self)->&'static pac::usart0::RegisterBlock{
     fn regs(&self)->&'static pac::flexcomm0::RegisterBlock{
         use FlexComm::*;
 
         match self{
-            eFLEXCOMM_HOST_UART => unsafe { &*(pac::Flexcomm0::ptr() as *const pac::flexcomm0::RegisterBlock)},
-            eFLEXCOMM_DEBUG_UART => unsafe { &*(pac::Flexcomm1::ptr() as *const pac::flexcomm0::RegisterBlock)},
-            eFLEXCOMM_VFG_I2C => unsafe { &*(pac::Flexcomm2::ptr() as *const pac::flexcomm0::RegisterBlock)},
-            eFLEXCOMM_TDM_UART => unsafe { &*(pac::Flexcomm3::ptr() as *const pac::flexcomm0::RegisterBlock)},
-            eFLEXCOMM_TOUCHPAD_UART => unsafe { &*(pac::Flexcomm4::ptr() as *const pac::flexcomm0::RegisterBlock)},
-            eFLEXCOMM_SURFLINK_UART => unsafe { &*(pac::Flexcomm5::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommHostUart => unsafe { &*(pac::Flexcomm0::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommDebugUart => unsafe { &*(pac::Flexcomm1::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommVfgI2c => unsafe { &*(pac::Flexcomm2::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommTdmUart => unsafe { &*(pac::Flexcomm3::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommTouchpadUart => unsafe { &*(pac::Flexcomm4::ptr() as *const pac::flexcomm0::RegisterBlock)},
+            FlexcommSurflinkUart => unsafe { &*(pac::Flexcomm5::ptr() as *const pac::flexcomm0::RegisterBlock)},
         }
     }
     
     pub fn set_peripheral(&self, peripheral:PeripheralType, lock: bool) -> bool {
-// todo: Ask if I can change peripheral from u8 to "enum Persel"
-        if (peripheral != PeripheralType::NoPeriphSelected){
-            if (peripheral == PeripheralType::I2c && self.regs().pselid().read().i2cpresent().is_not_present()){
+        if peripheral != PeripheralType::NoPeriphSelected{
+            if peripheral == PeripheralType::I2c && self.regs().pselid().read().i2cpresent().is_not_present(){
                 return false;
                 //todo add a panic here
-            } else if (peripheral == PeripheralType::Spi && self.regs().pselid().read().spipresent().is_not_present()){
+            } else if peripheral == PeripheralType::Spi && self.regs().pselid().read().spipresent().is_not_present(){
                 return false;
                 //todo add a panic here
-            } else if (peripheral == PeripheralType::Usart && self.regs().pselid().read().usartpresent().is_not_present()){
+            } else if peripheral == PeripheralType::Usart && self.regs().pselid().read().usartpresent().is_not_present(){
                 return false;
                 //todo add a panic here
-            }else if ((peripheral == PeripheralType::I2sReceive ||  peripheral == PeripheralType::I2sTransmit) && self.regs().pselid().read().i2spresent().is_not_present()){
+            }else if (peripheral == PeripheralType::I2sReceive ||  peripheral == PeripheralType::I2sTransmit) && self.regs().pselid().read().i2spresent().is_not_present(){
                 return false;
                 //todo add a panic here
             }
@@ -113,7 +123,7 @@ impl FlexComm{
         //self.regs().pselid().read().persel().into().
         //self.regs().pselid().read().persel().ne(&peripheral);
         
-        if (lock ){
+        if lock {
             self.regs().pselid().write(|w| w.lock().locked());
         } else {
             self.regs().pselid().write(|w| w.lock().unlocked());
@@ -123,12 +133,160 @@ impl FlexComm{
         return true;
     }
 }
+#[derive(Debug, PartialEq, Eq)]
+pub enum UartParity{
+    None,
+    Even,
+    Odd,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum UartBitsPerCharacter{
+    Seven,
+    Eight,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum UartStopBits{
+    One,
+    Two,
+}
 
 pub struct Uart{
-    baudrate: u32,
     flexcomm: FlexComm,
+    baudrate: u32,
+    bitsPerCharacter: UartBitsPerCharacter,
+    parity: UartParity,
+    stopBits: UartStopBits,
+    controlFlags: u32,
+    statusFlags: Cell<u32>,
+    interruptPri:u16
     //dma: dma0::RegisterBlock,
     //tx_dma: dma0::RegisterBlock,
     //rx_dma: dma0::RegisterBlock,
 
+}
+
+impl Uart{
+
+    fn reg(&self)->&'static pac::usart0::RegisterBlock{
+        unsafe { &*(pac::Usart0::ptr() as *const pac::usart0::RegisterBlock)}
+    }
+
+    pub fn new(flexcomm: FlexComm, baudrate: u32, bitsPerCharacter: UartBitsPerCharacter, parity: UartParity, stopBits: UartStopBits, controlFlags:u32,_statusFlags:u32,interruptPri:u16 )->Self{
+        Uart{
+            flexcomm,
+            baudrate,
+            bitsPerCharacter,
+            parity,
+            stopBits,
+            controlFlags,
+            statusFlags:Cell::new(_statusFlags),
+            interruptPri
+        }
+    }
+
+    pub fn init(&self){
+        //self.flexcomm.set_peripheral(PeripheralType::Usart, true);
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+
+        // Todo: Add code to abort DMA transfer of Tx and Rx
+        // todo: Add code to save the interrupt ID, prio and target ID
+
+        if self.bitsPerCharacter == UartBitsPerCharacter::Seven{
+            self.reg().cfg().write(|w| w.datalen().bit_7());
+        }
+        else{
+            //self.bitsPerCharacter == UartBitsPerCharacter::Eight
+            self.reg().cfg().write(|w| w.datalen().bit_8());
+        }
+
+        if self.parity == UartParity::None{
+            self.reg().cfg().write(|w| w.paritysel().no_parity());
+        } else if self.parity == UartParity::Even{
+            self.reg().cfg().write(|w| w.paritysel().even_parity());
+        } else if self.parity == UartParity::Odd{
+            self.reg().cfg().write(|w| w.paritysel().odd_parity());
+        }
+
+        if self.stopBits == UartStopBits::One{
+            self.reg().cfg().write(|w| w.stoplen().bit_1());
+        } else {
+            //UartStopBits::Two
+            self.reg().cfg().write(|w| w.stoplen().bits_2());
+        }
+
+        // TODO : Set Nvic priority : NVIC_SetPriority((IRQn_Type)pTgtPrivCtxt->interruptId, pTgtPrivCtxt->interruptPri);
+
+
+
+    }
+
+    pub fn uart_open(&self, clkFreq_Hz :u32){
+        //self.flexcomm.set_peripheral(PeripheralType::Usart, true);
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+        self.uart_calculate_baud_rate(clkFreq_Hz);
+
+        // TODO: Add code to abort the DMA's on Rx and Tx lines
+        // TODO: Add code to enable Flexcomm Uart DMA.
+
+        if self.controlFlags & UART_CTRL_FLAG_ENABLE_TX != 0{
+            self.reg().fifocfg().write(|w| w.enabletx().set_bit());
+            self.reg().fifocfg().write(|w| w.emptytx().set_bit());
+        }
+
+        if self.controlFlags & UART_CTRL_FLAG_ENABLE_RX != 0{
+            self.reg().fifocfg().write(|w| w.emptyrx().set_bit());
+            self.reg().fifocfg().write(|w| w.enablerx().set_bit());
+            self.reg().fifocfg().write(|w| w.dmarx().set_bit());
+
+            //TODO: Start the DMA transfer
+        }
+
+        //Clear the FIFO error
+        self.reg().fifostat().write(|w| w.txerr().set_bit());
+        self.reg().fifostat().write(|w| w.rxerr().set_bit());
+
+        self.reg().intenset().write(|w| w.starten().set_bit());
+        self.reg().intenset().write(|w| w.deltarxbrken().set_bit());
+        self.reg().intenset().write(|w| w.framerren().set_bit());
+        self.reg().intenset().write(|w| w.parityerren().set_bit());
+
+        // TODO: Enable NVIC interrupt : NVIC_EnableIRQ((IRQn_Type)pTgtPrivCtxt->interruptId);
+
+        self.statusFlags.set(self.statusFlags.get() | UART_STAT_FLAG_FLAG_OPEN);
+
+    }
+
+    pub fn uart_close(&self){
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().disabled());
+    }
+
+    pub fn uart_wakeup(&self){
+        //self.flexcomm.regs().ctrl().write(|w| w.enable().enabled());
+    }
+
+    pub fn uart_sleep(&self){
+       // self.flexcomm.regs().ctrl().write(|w| w.enable().disabled());
+    }
+
+    fn uart_calculate_baud_rate(&self,_clkFreq_Hz :u32){
+        let mut best_osrval:u8 = 0xf;
+        let mut best_brgval:u16 = 0xffff;
+       // TODO: add code to update values of OSR and BRG reg.
+
+       best_osrval = 0xf;
+       best_brgval = 0xffff;
+       let _baudrate = self.baudrate;
+
+       unsafe{
+           self.reg().osr().write(|w| w.osrval().bits(best_osrval));
+           self.reg().brg().write(|w| w.brgval().bits(best_brgval));
+       }
+    }
 }
