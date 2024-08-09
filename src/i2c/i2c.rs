58d2ddcc585ae50821d14b7a8a3b4ac4f69a109d
Scott Fudally <sfudally@microsoft.com>
2024-08-09 12:50:56 -0700

    Ongoing implementation

--- src/i2c/i2c.rs
+++ src/i2c/i2c.rs
@@ -32,16 +32,48 @@ impl<'d, T: Instance> I2c<'d, T> {
 }
 
 impl<'d, T: Instance> embedded_hal_1::i2c::I2c for I2c<'d, T> {
-    fn read(&mut self, _address: u8, _read: &mut [u8]) -> Result<(), Self::Error> {
-        todo!();
+    fn read(&mut self, address: u8, read: &mut [u8]) -> Result<(), Self::Error> {
+        // Procedure from 24.3.1.2 pg 546
+        let i2cregs = T::i2c_regs();
+        i2cregs.cfg().write(|w| w.msten().set_bit());
+
+        i2cregs.mstdat().write(|w| unsafe { w.data().bits(address | 0x80) });
+        i2cregs.mstctl().write(|w| w.mststart().set_bit());
+        while i2cregs.stat().read().mstpending().bit_is_clear() {}
+
+        for index in 0..read.len() {
+            while i2cregs.stat().read().mstpending().bit_is_clear() {}
+            read[index] = (i2cregs.mstdat().read().bits() & 0xFF) as u8;
+        }
+
+        i2cregs.mstctl().write(|w| w.mststop().set_bit());
+
+        Ok(())
     }
 
-    fn write(&mut self, _address: u8, _write: &[u8]) -> Result<(), Self::Error> {
-        todo!();
+    fn write(&mut self, address: u8, write: &[u8]) -> Result<(), Self::Error> {
+        // Procedure from 24.3.1.1 pg 545
+        let i2cregs = T::i2c_regs();
+        i2cregs.cfg().write(|w| w.msten().set_bit());
+
+        i2cregs.mstdat().write(|w| unsafe { w.data().bits(address & 0x7F) });
+        i2cregs.mstctl().write(|w| w.mststart().set_bit());
+        while i2cregs.stat().read().mstpending().bit_is_clear() {}
+
+        for byte in write.iter() {
+            i2cregs.mstdat().write(|w| unsafe { w.data().bits(*byte) });
+            while i2cregs.stat().read().mstpending().bit_is_clear() {}
+        }
+
+        i2cregs.mstctl().write(|w| w.mststop().set_bit());
+
+        Ok(())
     }
 
-    fn write_read(&mut self, _address: u8, _write: &[u8], _read: &mut [u8]) -> Result<(), Self::Error> {
-        todo!();
+    fn write_read(&mut self, address: u8, write: &[u8], read: &mut [u8]) -> Result<(), Self::Error> {
+        self.write(address, write)?;
+        self.read(address, read)?;
+        Ok(())
     }
 
     fn transaction(
