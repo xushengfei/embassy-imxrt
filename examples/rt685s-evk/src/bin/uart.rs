0134c6c02ce2e27f02cd742a23dd259ed725d206
David Marks <davmark@microsoft.com>
2024-08-29 15:02:29 -0700

    wip:fc example2

--- examples/rt685s-evk/src/bin/uart.rs
+++ examples/rt685s-evk/src/bin/uart.rs
@@ -8,16 +8,20 @@ use embassy_imxrt::flexcomm::Config as FcConfig;
 use embassy_imxrt::flexcomm::Flexcomm;
 use embassy_imxrt::uart::Config;
 use embassy_imxrt::uart::Uart;
+use mimxrt685s_pac as pac;
+
+pub use pac::flexcomm0::pselid::Lock as FlexcommLock;
+pub use pac::flexcomm0::pselid::Persel as FcFunction;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_imxrt::init(Default::default());
 
     let mut fc0_config = FcConfig::default();
-    fc0_config.function = flexcomm::Function::Usart;
-    fc0_config.lock = flexcomm::FlexcommLock::Unlocked;
+    fc0_config.function = FcFunction::Usart;
+    fc0_config.lock = FlexcommLock::Unlocked;
 
-    fc0 = flexcomm::Flexcomm0.new(&fc0_config);
+    let fc0 = <dyn Flexcomm>::Flexcomm0.new(&fc0_config);
     fc0.enable(&fc0_config);
 
     let config = Config::default();
