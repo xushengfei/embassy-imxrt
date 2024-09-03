47a289af7407792f79fb9f121b271b2b94764d1a
Akshay Mahajan <akmahaj@microsoft.com>
2024-09-03 14:12:54 +0530

    Updated uart example

--- examples/rt685s-evk/src/bin/uart.rs
+++ examples/rt685s-evk/src/bin/uart.rs
@@ -3,24 +3,32 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-//use embassy_imxrt::uart::*;
+use embassy_imxrt::uart::*;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_imxrt::init(Default::default());
+    let _p = embassy_imxrt::init(Default::default());
 
     {
-        let config = Config::default();
-        let mut usart = Uart::new_blocking(&mut usart, &mut rx, &mut tx, config).unwrap(); //toto change
+        //let config = Config::default();
+        //let mut usart = Uart::new_blocking(&mut usart, &mut rx, &mut tx, config).unwrap(); //toto change
+
+        let mut usart = UartRxTx::new();
+        usart.init();
 
         // We can't send too many bytes, they have to fit in the FIFO.
         // This is because we aren't sending+receiving at the same time.
 
-        let data = [0xC0, 0xDE];
-        usart.blocking_write(&data).unwrap();
+        //let data = [0xC0, 0xDE];
+        //usart.blocking_write(&data).unwrap();
+        let mut data = [0xC0, 0xDE];
+        usart.write_blocking(&mut data, 2);
 
         let mut buf = [0; 2];
-        usart.blocking_read(&mut buf).unwrap();
+        //usart.blocking_read(&mut buf).unwrap();
+        usart.read_blocking(&mut buf, 2);
         assert_eq!(buf, data);
+
+        usart.deinit();
     }
 }
