ad377fc4ca001690c46a371d9822aabebf56674c
Madeleyne Vaca <mavaca@microsoft.com>
2024-09-30 10:04:05 -0700

    Create clocks-blinky example which shows different clocks on the CLKOUT pin
    Through this example, the CLKOUT pin (P1_10) shows the frequency of
    different clocks as they're initialized in the clocks module. This can
    be used to verify or tune values like PFDn or FFRO tempco/trim-fine

---
+++ examples/rt685s-evk/src/bin/clocks-blinky.rs
@@ -0,0 +1,71 @@
+#![no_main]
+#![no_std]
+
+use defmt::{error, info};
+use embassy_executor::Spawner;
+use embassy_imxrt::clocks;
+use embassy_imxrt::gpio;
+use embassy_imxrt::iopctl::IopctlPin;
+use embassy_time::Timer;
+use {defmt_rtt as _, embassy_imxrt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let embassy_p = embassy_imxrt::init(Default::default());
+
+    info!("Initializing GPIO");
+    unsafe { gpio::init() };
+
+    let mut led = gpio::Output::new(
+        embassy_p.PIO0_26,
+        gpio::Level::Low,
+        gpio::DriveMode::PushPull,
+        gpio::DriveStrength::Normal,
+        gpio::SlewRate::Standard,
+    );
+
+    let clk_out = embassy_p.PIO1_10;
+
+    clk_out
+        .disable_analog_multiplex()
+        .disable_input_buffer()
+        .set_drive_mode(embassy_imxrt::gpio::DriveMode::PushPull)
+        .set_drive_strength(embassy_imxrt::gpio::DriveStrength::Normal)
+        .set_input_polarity(embassy_imxrt::gpio::Polarity::ActiveHigh)
+        .set_function(embassy_imxrt::gpio::Function::F7)
+        .set_slew_rate(embassy_imxrt::gpio::SlewRate::Standard)
+        .set_pull(embassy_imxrt::gpio::Pull::None);
+
+    let mut clk_out_config = clocks::ClockOutConfig::default_config();
+    if let Err(e) = clk_out_config.enable_and_reset() {
+        error!("Couldn't enable clock out {:?}", e);
+    }
+
+    if let Err(e) = clk_out_config.set_clkout_source_and_div(clocks::ClkOutSrc::Lposc, 0) {
+        error!("Couldn't configure clock out {:?}", e);
+    }
+    info!("Clock out to LPOSC so 1MHz");
+    for _i in 0..10 {
+        led.toggle();
+        Timer::after_millis(1000).await;
+    }
+
+    if let Err(e) = clk_out_config.set_clkout_source_and_div(clocks::ClkOutSrc::Sfro, 31) {
+        error!("Couldn't configure clock out {:?}", e);
+    }
+    info!("switched Clock out to SFRO divided by 32 so 500KHz");
+    for _i in 0..10 {
+        led.toggle();
+        Timer::after_millis(1000).await;
+    }
+
+    if let Err(e) = clk_out_config.set_clkout_source_and_div(clocks::ClkOutSrc::MainClk, 99) {
+        error!("Couldn't configure clockout {:?}", e);
+    }
+    info!("switched Clock out to Main Clk div 100, expecting 5MHz");
+    loop {
+        info!("Toggling LED");
+        led.toggle();
+        Timer::after_millis(1000).await;
+    }
+}
