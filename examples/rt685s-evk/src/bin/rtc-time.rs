6fc853f2a297b86c2820b12f031424cdfb789ba6
Raghav Sankrantipati <sasankra@microsoft.com>
2024-10-04 16:39:23 -0700

    add prints

--- examples/rt685s-evk/src/bin/rtc-time.rs
+++ examples/rt685s-evk/src/bin/rtc-time.rs
@@ -11,7 +11,7 @@ async fn main(_spawner: Spawner) {
     let _p = embassy_imxrt::init(Default::default());
     let r = RtcDatetime::new();
 
-    let mut datetime = Datetime {
+    let datetime = Datetime {
         year: 2024,
         month: 10,
         day: 4,
@@ -20,15 +20,18 @@ async fn main(_spawner: Spawner) {
         second: 0,
     };
     let ret = r.set_datetime(&datetime);
+    info!("RTC set time: {:?}", datetime);
     // check if the set is valid
     assert!(ret == DatetimeResult::ValidDatetime);
 
+    info!("Wait fo 20 seconds");
     //wait for 20 seconds
     Timer::after_millis(20000).await;
 
     // get the datetime set and compare
-    let time = r.get_datetime();
-    info!("RTC time is {:?}", time);
+    let (time, result) = r.get_datetime();
+    assert!(result == DatetimeResult::ValidDatetime);
+    info!("RTC get time: {:?}", time);
 
     embassy_imxrt_examples::delay(50000);
 }
