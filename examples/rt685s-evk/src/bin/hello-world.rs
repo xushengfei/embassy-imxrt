4c7c96228278adc744989160cbf8f94d6e38dfe6
Felipe Balbi <febalbi@microsoft.com>
2024-10-21 08:58:18 -0700

    examples: run cargo +nightly clippy --fix
    Signed-off-by: Felipe Balbi <febalbi@microsoft.com>

--- examples/rt685s-evk/src/bin/hello-world.rs
+++ examples/rt685s-evk/src/bin/hello-world.rs
@@ -6,7 +6,7 @@ extern crate embassy_imxrt_examples;
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::Timer;
-use {defmt_rtt as _, embassy_imxrt, panic_probe as _};
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
