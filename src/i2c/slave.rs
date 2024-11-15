83241bef6d714585de11ed44e4dbb888c503d573
Felipe Balbi <febalbi@microsoft.com>
2024-11-14 16:33:10 -0800

    i2c: slave: pins must be impl Peripheral too
    By making sure pins are `impl Peripheral', we can pass mutable
    references to pins instead of consuming pins on the application
    layer. This would let one use the flexcomm for different modes within
    a single application as long as the rules for mutable references are
    obeyed.

--- src/i2c/slave.rs
+++ src/i2c/slave.rs
@@ -78,13 +78,15 @@ impl<'a, M: Mode> I2cSlave<'a, M> {
     /// use flexcomm fc with Pins scl, sda as an I2C Master bus, configuring to speed and pull
     fn new_inner<T: Instance>(
         _bus: impl Peripheral<P = T> + 'a,
-        scl: impl SclPin<T> + 'a,
-        sda: impl SdaPin<T> + 'a,
+        scl: impl Peripheral<P = impl SclPin<T>> + 'a,
+        sda: impl Peripheral<P = impl SdaPin<T>> + 'a,
         // TODO - integrate clock APIs to allow dynamic freq selection | clock: crate::flexcomm::Clock,
         address: Address,
         dma_ch: Option<dma::channel::ChannelAndRequest<'a>>,
     ) -> Result<Self> {
         into_ref!(_bus);
+        into_ref!(scl);
+        into_ref!(sda);
 
         sda.as_sda();
         scl.as_scl();
@@ -129,8 +131,8 @@ impl<'a> I2cSlave<'a, Blocking> {
     /// use flexcomm fc with Pins scl, sda as an I2C Master bus, configuring to speed and pull
     pub fn new_blocking<T: Instance, D: dma::Instance>(
         _bus: impl Peripheral<P = T> + 'a,
-        scl: impl SclPin<T> + 'a,
-        sda: impl SdaPin<T> + 'a,
+        scl: impl Peripheral<P = impl SclPin<T>> + 'a,
+        sda: impl Peripheral<P = impl SdaPin<T>> + 'a,
         // TODO - integrate clock APIs to allow dynamic freq selection | clock: crate::flexcomm::Clock,
         address: Address,
         _dma_ch: impl Peripheral<P = D> + 'a,
@@ -168,8 +170,8 @@ impl<'a> I2cSlave<'a, Async> {
     /// use flexcomm fc with Pins scl, sda as an I2C Master bus, configuring to speed and pull
     pub fn new_async<T: Instance, D: dma::Instance>(
         _bus: impl Peripheral<P = T> + 'a,
-        scl: impl SclPin<T> + 'a,
-        sda: impl SdaPin<T> + 'a,
+        scl: impl Peripheral<P = impl SclPin<T>> + 'a,
+        sda: impl Peripheral<P = impl SdaPin<T>> + 'a,
         _irq: impl interrupt::typelevel::Binding<T::Interrupt, InterruptHandler<T>> + 'a,
         // TODO - integrate clock APIs to allow dynamic freq selection | clock: crate::flexcomm::Clock,
         address: Address,
