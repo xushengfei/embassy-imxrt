f277280bce911f7de06d444af9cb4813d5d21357
Jeff Glaum <jeffglaum@live.com>
2024-11-18 07:49:02 -0800

    examples: dma-mem: remove RNG dependency
    Remove the dependency on the RNG peripheral when testing the DMA
    block.

--- examples/rt685s-evk/src/bin/dma-mem.rs
+++ examples/rt685s-evk/src/bin/dma-mem.rs
@@ -11,7 +11,7 @@ use {defmt_rtt as _, panic_probe as _};
 const TEST_LEN: usize = 16;
 
 macro_rules! test_dma_channel {
-    ($peripherals: expr, $rng: expr, $instance: ident, $number: expr) => {
+    ($peripherals:expr, $instance:ident, $number:expr) => {
         let ch = Dma::reserve_channel::<$instance>($peripherals.$instance);
 
         for width in [Width::Bit8, Width::Bit16, Width::Bit32] {
@@ -48,38 +48,38 @@ async fn main(_spawner: Spawner) {
 
     info!("Test memory-to-memory DMA transfers");
 
-    test_dma_channel!(p, rng, DMA0_CH0, 0);
-    test_dma_channel!(p, rng, DMA0_CH1, 1);
-    test_dma_channel!(p, rng, DMA0_CH2, 2);
-    test_dma_channel!(p, rng, DMA0_CH3, 3);
-    test_dma_channel!(p, rng, DMA0_CH4, 4);
-    test_dma_channel!(p, rng, DMA0_CH5, 5);
-    test_dma_channel!(p, rng, DMA0_CH6, 6);
-    test_dma_channel!(p, rng, DMA0_CH7, 7);
-    test_dma_channel!(p, rng, DMA0_CH8, 8);
-    test_dma_channel!(p, rng, DMA0_CH9, 9);
-    test_dma_channel!(p, rng, DMA0_CH10, 10);
-    test_dma_channel!(p, rng, DMA0_CH11, 11);
-    test_dma_channel!(p, rng, DMA0_CH12, 12);
-    test_dma_channel!(p, rng, DMA0_CH13, 13);
-    test_dma_channel!(p, rng, DMA0_CH14, 14);
-    test_dma_channel!(p, rng, DMA0_CH15, 15);
-    test_dma_channel!(p, rng, DMA0_CH16, 16);
-    test_dma_channel!(p, rng, DMA0_CH17, 17);
-    test_dma_channel!(p, rng, DMA0_CH18, 18);
-    test_dma_channel!(p, rng, DMA0_CH19, 19);
-    test_dma_channel!(p, rng, DMA0_CH20, 20);
-    test_dma_channel!(p, rng, DMA0_CH21, 21);
-    test_dma_channel!(p, rng, DMA0_CH22, 22);
-    test_dma_channel!(p, rng, DMA0_CH23, 23);
-    test_dma_channel!(p, rng, DMA0_CH24, 24);
-    test_dma_channel!(p, rng, DMA0_CH25, 25);
-    test_dma_channel!(p, rng, DMA0_CH26, 26);
-    test_dma_channel!(p, rng, DMA0_CH27, 27);
-    test_dma_channel!(p, rng, DMA0_CH28, 28);
-    test_dma_channel!(p, rng, DMA0_CH29, 29);
-    test_dma_channel!(p, rng, DMA0_CH30, 30);
-    test_dma_channel!(p, rng, DMA0_CH31, 31);
+    test_dma_channel!(p, DMA0_CH0, 0);
+    test_dma_channel!(p, DMA0_CH1, 1);
+    test_dma_channel!(p, DMA0_CH2, 2);
+    test_dma_channel!(p, DMA0_CH3, 3);
+    test_dma_channel!(p, DMA0_CH4, 4);
+    test_dma_channel!(p, DMA0_CH5, 5);
+    test_dma_channel!(p, DMA0_CH6, 6);
+    test_dma_channel!(p, DMA0_CH7, 7);
+    test_dma_channel!(p, DMA0_CH8, 8);
+    test_dma_channel!(p, DMA0_CH9, 9);
+    test_dma_channel!(p, DMA0_CH10, 10);
+    test_dma_channel!(p, DMA0_CH11, 11);
+    test_dma_channel!(p, DMA0_CH12, 12);
+    test_dma_channel!(p, DMA0_CH13, 13);
+    test_dma_channel!(p, DMA0_CH14, 14);
+    test_dma_channel!(p, DMA0_CH15, 15);
+    test_dma_channel!(p, DMA0_CH16, 16);
+    test_dma_channel!(p, DMA0_CH17, 17);
+    test_dma_channel!(p, DMA0_CH18, 18);
+    test_dma_channel!(p, DMA0_CH19, 19);
+    test_dma_channel!(p, DMA0_CH20, 20);
+    test_dma_channel!(p, DMA0_CH21, 21);
+    test_dma_channel!(p, DMA0_CH22, 22);
+    test_dma_channel!(p, DMA0_CH23, 23);
+    test_dma_channel!(p, DMA0_CH24, 24);
+    test_dma_channel!(p, DMA0_CH25, 25);
+    test_dma_channel!(p, DMA0_CH26, 26);
+    test_dma_channel!(p, DMA0_CH27, 27);
+    test_dma_channel!(p, DMA0_CH28, 28);
+    test_dma_channel!(p, DMA0_CH29, 29);
+    test_dma_channel!(p, DMA0_CH30, 30);
+    test_dma_channel!(p, DMA0_CH31, 31);
 
     info!("DMA transfer tests completed");
 }
