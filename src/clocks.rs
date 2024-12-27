d346db30133f10e3df5afcf114a45e69c410766b
jerrysxie <jerryxie@microsoft.com>
2024-12-27 12:58:20 -0600

    clock: clean up rtc init code
    - use invariant accessors
    
    - combine 2 read-modify-writes into 1
    
    - change modify for single bitfield register into writes

--- src/clocks.rs
+++ src/clocks.rs
@@ -1163,21 +1163,21 @@ impl RtcClkConfig {
         let cc1 = unsafe { pac::Clkctl1::steal() };
         let r = unsafe { pac::Rtc::steal() };
         // Enable the RTC peripheral clock
-        cc1.pscctl2().modify(|_r, w| w.rtc_lite_clk().enable_clock());
-        // Make sure the reset bit is cleared
-        r.ctrl().modify(|_r, w| w.swreset().clear_bit());
-        // Make sure the RTC OSC is powered up
-        r.ctrl().modify(|_r, w| w.rtc_osc_pd().clear_bit());
+        cc1.pscctl2_set().write(|w| w.rtc_lite_clk_set().set_clock());
+        // Make sure the reset bit is cleared amd RTC OSC is powered up
+        r.ctrl().modify(|_, w| w.swreset().not_in_reset().rtc_osc_pd().enable());
+
         // set initial match value, note that with a 15 bit count-down timer this would
         // typically be 0x8000, but we are "doing some clever things" in time-driver.rs,
         // read more about it in the comments there
         // SAFETY: unsafe needed to write the bits
-        r.wake().modify(|_r, w| unsafe { w.bits(0xA) });
+        r.wake().write(|w| unsafe { w.bits(0xA) });
+
         // Enable 32K OSC
-        cc0.osc32khzctl0().modify(|_r, w| w.ena32khz().set_bit());
+        cc0.osc32khzctl0().write(|w| w.ena32khz().enabled());
 
         // enable rtc clk
-        r.ctrl().modify(|_r, w| w.rtc_en().set_bit());
+        r.ctrl().modify(|_, w| w.rtc_en().enable());
     }
 }
 
