f5b5a2a0fc16928c0011f9f102e3430db5edf351
jerrysxie <jerryxie@microsoft.com>
2025-01-10 21:00:56 -0600

    Add benchmarking using gpio pin toggling example

---
+++ examples/rt685s-evk/src/bin/benchmarking-gpio.rs
@@ -0,0 +1,53 @@
+#![no_std]
+#![no_main]
+
+extern crate embassy_imxrt_examples;
+
+use defmt::info;
+use embassy_executor::Spawner;
+use embassy_imxrt::gpio;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_imxrt::init(Default::default());
+
+    info!("Benchmarking using gpio pins");
+
+    // Use a pin that is not in use
+    // Initialize the pin to make sure it is configured correctly
+    let mut _output = gpio::Output::new(
+        p.PIO1_0,
+        gpio::Level::Low,
+        gpio::DriveMode::PushPull,
+        gpio::DriveStrength::Normal,
+        gpio::SlewRate::Standard,
+    );
+
+    let n = 1000;
+
+    // Steal the GPIO register map
+    let gpio = unsafe { embassy_imxrt::pac::Gpio::steal() };
+
+    loop {
+        // Do direct write to toggle the pin
+        gpio.not(1).write(|w| unsafe { w.notp().bits(1 << 0) });
+
+        // Operation to be benchmarked
+        for _h in 0..n {
+            cortex_m::asm::nop();
+        }
+
+        // Do direct write to toggle the pin
+        gpio.not(1).write(|w| unsafe { w.notp().bits(1 << 0) });
+
+        // Operation to be benchmarked
+        for _l in 0..n {
+            cortex_m::asm::nop();
+        }
+
+        /* Measure pulse width of pin using a scope to benchmark
+           Mutiple pins can be used to get the best measurement
+        */
+    }
+}
